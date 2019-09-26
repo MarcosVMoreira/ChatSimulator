853ccf71be4338e983c94aa043e10e3ab4436efc
Marcos Moreira <contato.marcosvmoreira@gmail.com>
2019-09-26 17:24:27 -0300

    Add: client login method

--- UDPClient.java
+++ UDPClient.java
@@ -7,48 +7,30 @@ import com.google.gson.Gson;
 import java.io.*;
 
 public class UDPClient {
+	
+	
+	
     public static void main(String args[]) {
         // args fornece o conteúdo da mensagem e o nome de host do servidor
         DatagramSocket aSocket = null;
         
-        User user = new User();
-        Message message = new Message();
-        
-        Gson userJSON = new Gson();
-        Gson messageJSON = new Gson();
-        
-        String userString;
-        String messageString;
-
         Scanner s = new Scanner(System.in);
         
-        String userName;
+        String aux;
+
         
         while(true) {
         	try {
-            	System.out.print("Loging in. Inser your username: ");
+        		
+        		aSocket = new DatagramSocket();
             	
-            	
-            	userName = s.nextLine();
-            	
-            	user.setUserName(userName);
-            	
-            	userString = userJSON.toJson(user);
-            	
-            	System.out.println("Meu json criado: "+userJSON);
-            	
-            	message.setMessageCode(100);
-
-            	
-            	
-            	
-                aSocket = new DatagramSocket();
-                byte[] m = userName.getBytes();
-
+                System.out.println("Loging in. Inser your nickname: ");
+                
+                aux = s.next();
+                
                 InetAddress aHost = InetAddress.getLocalHost();
-                int serverPort = 6698;
-                DatagramPacket request = new DatagramPacket(m, m.length, aHost, serverPort);
-                aSocket.send(request);
+                
+                logIn(aux, aHost, 6698);
 
                 byte[] buffer = new byte[1000];
                 DatagramPacket receive = new DatagramPacket(buffer, buffer.length);
@@ -64,5 +46,57 @@ public class UDPClient {
             }
         }
         
+        
+        
     }
+    
+    public static void logIn (String userName, InetAddress host, int serverPort) {
+    	
+    	User user = new User();
+    	Message message = new Message();
+
+    	Gson gson = new Gson();
+
+    	user.setUserName(userName);
+
+    	message.setMessageCode(100);
+    	
+    	message.setMessageText(gson.toJson(user));
+    	
+    	System.out.println("Login JSON: "+gson.toJson(user));
+    	
+    	sendJson(gson.toJson(message), host, serverPort);
+    	
+    }
+    
+    
+    
+    public static void sendJson (String JSONInfo, InetAddress host, int serverPort) {
+    	
+    	DatagramSocket aSocket = null;
+    	Gson gson = new Gson();
+    	
+    	try {
+			aSocket = new DatagramSocket();
+			
+			byte[] m = JSONInfo.getBytes();
+
+	        DatagramPacket request = new DatagramPacket(m, m.length, host, serverPort);
+	        
+	        System.out.println("Sending packet to IP: "+host.getHostAddress()+" port: "+serverPort);
+	        
+	        aSocket.send(request);
+
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			 aSocket.close();
+		}
+
+    }
+    
 }
